78b20c4438a4698f5c84b955eb9d00b12ff4f24a
Axel <axosar@gmail.com>
2019-11-12 08:50:39 -0600

    Updates

---
+++ precache-manifest.04be95680d1fae375bcc2819ea2ce36c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0550473911b0826927dcb1a5e4f68860",
+    "url": "/macrofibras/index.html"
+  },
+  {
+    "revision": "66ff2ccdcfd543a2b944",
+    "url": "/macrofibras/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "e61258f5a63e45e45943",
+    "url": "/macrofibras/static/js/2.cf72863e.chunk.js"
+  },
+  {
+    "revision": "66ff2ccdcfd543a2b944",
+    "url": "/macrofibras/static/js/main.a21cb72b.chunk.js"
+  },
+  {
+    "revision": "2726681bc3013b169361",
+    "url": "/macrofibras/static/js/runtime-main.9d39a23d.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/macrofibras/static/media/logo.25bf045c.svg"
+  }
+]);
