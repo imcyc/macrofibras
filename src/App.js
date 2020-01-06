77f1b843b78a68de84ac61a3b5d74f99313bd704
Axel <axosar@gmail.com>
2020-01-06 11:59:42 -0600

    SELECCIONAR ESTRUCTURA

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
+import Button from 'react-bootstrap/Button'
 import './App.css';
 
 function App() {
+
+  const seleccionarEstructura = (e) => {
+    e.preventDefault();
+    console.log("hola");
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <h1>CALCULADORA DE MACROFIBRAS DE CONCRETO</h1>
+        <h1>CALCULADORA DE</h1>
+        <h2>MACROFIBRAS DE CONCRETO</h2>
         <p>Instituto Mexicano del Cemento y del Concreto A.C.</p>
+        <Button variant="dark" className="mt-3" onClick={seleccionarEstructura}>  
+          SELECCIONAR ESTRUCTURA
+        </Button>
       </header>
     </div>
   );
