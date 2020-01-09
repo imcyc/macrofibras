f492c193e968f8ea5ba5f4b1bb2f7e2cd2ad1a57
Axel <axosar@gmail.com>
2020-01-09 09:38:04 -0600

    Imagen cabezera

--- src/Components/Estructura.js
+++ src/Components/Estructura.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Titulo from './Titulo';
+import LOGOTexto from '../images/LOGOTexto.svg';
 import Seccion from './Seccion';
 import './Estructura.css';
 
 function Estructura(props) {
   return(
     <div className="wpestructuras">
-      <Titulo tituloEstructura={props.tituloEstructura} />
       <hr/>
+      <img src={LOGOTexto} alt="Logo" style={{width: '90%'}} />
       <div className="estructuras">
         {props.secciones.map((seccione, index) => (
           <Seccion key={index} nombre={seccione.nombre} imagen={seccione.imagen} enlace={'/tipo-de-acero'} />
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import Titulo from './Titulo';
+import calculadora2020 from '../images/calculadora2020.png';
 import {Link} from "react-router-dom";
 import Button from 'react-bootstrap/Button';
 
 function Home(props) {
   return (
     <div>
-      <Titulo />
       <Link to="/seleccionar-estructura">
-        <Button variant="dark" className="mt-3">  
-          SELECCIONAR ESTRUCTURA
-        </Button>
+      <img src={calculadora2020} alt="Logo" style={{width: '100%'}} />
       </Link>
     </div>
   )
